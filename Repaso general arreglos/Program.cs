fb4647e8444b482e1dcb96a8d25a0fcd4b34ae7f
JepCast <ycastillop2@miumg.edu.gt>
2023-04-01 09:45:01 -0600

    Agregar archivos de proyecto.

---
+++ Repaso general arreglos/Program.cs
@@ -0,0 +1,40 @@
+﻿
+void matriz ()
+{
+    string[] nombre = new string[]
+    {
+        "Anderson", "Brandon", "Yeimi", "Fatima", "Vivian"
+    };
+    
+    
+   foreach(string n in nombre)
+   {
+    Console.WriteLine("nombre=" + n);
+   }
+
+    int[] notas = new int[]
+    {
+        50,90,85,73,95
+    };
+
+    //int promedio = 0;
+    //foreach (int n in notas)
+    //{ 
+    //    promedio= promedio + n;
+    //};
+
+    //promedio = promedio / notas.Length;
+    //Console.WriteLine("\nEl promedio de las notas es:" + promedio);
+
+   
+    int mayor = 0;
+    for (int i = 0; i < 5; i++)
+    {
+        if (notas > mayor)
+        {
+            mayor = notas;
+        }
+    }
+    Console.WriteLine("El numero mayor es: " + mayor);
+}
+matriz();
